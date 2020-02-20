88b78f7e4d899e1e9443aae17259a53c6f8a453c
Mohamed_Elfakharany <mohmedelfakharany@gmail.com>
2020-02-20 17:29:27 +0200

    SearchCity&SpecialityDesignDone100%

---
+++ IMMUNITY/Patient/Search/BookingConfirmationVC.swift
@@ -0,0 +1,49 @@
+//
+//  BookingConfirmationVC.swift
+//  IMMUNITY
+//
+//  Created by elfakharany on 2/20/20.
+//  Copyright © 2020 Mohamed Elfakharany. All rights reserved.
+//
+
+import UIKit
+
+class BookingConfirmationVC: UIViewController {
+    
+    @IBOutlet weak var BtnMyAppointmentOutlet:UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        gradBTNS()
+        
+    }
+    func gradBTNS() {
+        
+        let RightGradientColor = #colorLiteral(red: 0.9333333333, green: 0.5294117647, blue: 0.537254902, alpha: 1)
+        let LiftGradientColor = #colorLiteral(red: 0.9647058824, green: 0.7960784314, blue: 0.7921568627, alpha: 1)
+        // First BTN
+        let gradientLayer = CAGradientLayer()
+        
+        gradientLayer.frame = BtnMyAppointmentOutlet.bounds
+        
+        gradientLayer.colors = [RightGradientColor.cgColor, LiftGradientColor.cgColor]
+        
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        
+        BtnMyAppointmentOutlet.layer.insertSublayer(gradientLayer, at: 0)
+        
+        BtnMyAppointmentOutlet.layer.cornerRadius = 10
+        BtnMyAppointmentOutlet.clipsToBounds = true
+        
+    }
+    
+    @IBAction func BtnMyAppointmentAction (_ sender : Any) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "MyAppointmentVC") as? MyAppointmentVC {
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
+    
+}
